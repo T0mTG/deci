ee0db6b4de0730e2a4f2ae0cf43cefa081ff6d17
T0mTG <dogiaminh0612@gmail.com>
2025-12-02 10:30:06 -0600

    add-delete-atr-button

--- frontend/app/ui/table_vert.tsx
+++ frontend/app/ui/table_vert.tsx
@@ -6,13 +6,8 @@ import { postTableFill,postSaveJson, getInitJson } from "../action/dataManager"
 
 
 export default function TableDispVert(){
-    const [cell_val,setCellVal]=useState("")
-    const [cell_key,setCellKey]=useState(0)
-    const [obj_atr,setObjAtr]=useState("")
-    const [obj_id,setObjID]=useState(0)
-    const [atrCount,setCount]=useState(4)
-    const [table_data,setTableData]=useState([{"atr1": "", "atr2": ""}, {"atr1": "", "atr2": ""}, {"atr1": "", "atr2": ""}]);
 
+    const [table_data,setTableData]=useState([{"atr0": "", "atr1": ""}, {"atr0": "", "atr1": ""}, {"atr0": "", "atr1": ""}]);
     const [atr_data,setAtrData]=useState([
         "", ""
     ]);
@@ -23,37 +18,23 @@ export default function TableDispVert(){
         setAtrData(tmp);
     }
 
-    //revampe done
+    //revamp done
     const HandleAddCol =() =>{
         const empty_obj: Record<string,string>={};
         atr_data.forEach((a,i)=>{
-            empty_obj["atr"+(i+1)]=""
+            empty_obj["atr"+(i)]=""
         });
         setTableData((prev)=>[...prev,empty_obj]) 
         console.log(table_data);
     }
 
 
-    //enter can be worked on later
-    const EnterPressed_col = (event: React.KeyboardEvent<HTMLInputElement>)=>{
-        if(event.key==="Enter"){
-            event.preventDefault();
-            let tmp=table_data;
-            tmp[obj_id][obj_atr]=cell_val;
-            setTableData(tmp);
-            // event.preventDefault();
-            // setAtr_data(prev => ({
-            //     ...prev,
-            //     [cell_key]: cell_val
-            // }));
-        }
-    }
+    //work on enter pressing
     
     //revamp add row
     const HandleAddRow=() =>{
-        const newAtr="atr"+atrCount;
-        setCount((prev)=>prev+1);
-        setAtr_data((prev)=>[...prev,""]);
+        const newAtr="atr"+(atr_data.length);
+        setAtrData((prev)=>[...prev,""]);
         setTableData((prev)=>
             prev.map((obj)=>({
                 ...obj,
@@ -63,9 +44,9 @@ export default function TableDispVert(){
     }
         
     //revamp cell change
-    const HandleCellChange=(key_obj:number, table_atr:string, val:string)=>{
+    const HandleCellChange=(obj_key:number, table_atr:string, val:string)=>{
         const updated=[...table_data];
-        updated[key_obj]={...updated[key_obj],[table_atr]:val};
+        updated[obj_key]={...updated[obj_key],[table_atr]:val};
         setTableData(updated);
     }
 
@@ -94,29 +75,56 @@ export default function TableDispVert(){
     useEffect(()=>{HandleInitTable();
     }, []);  
 
+    const HandleTest=(atr_id:number)=>{
+        console.log(atr_id)
+        let tmp:string[]=[];
+        const tmp_table=[...table_data]
+
+        if(atr_id+1<atr_data.length){
+            let del_atr="atr"+(atr_data.length-1)
+            console.log(del_atr)
+            tmp_table.forEach((obj)=>{
+                for(let i=atr_id;i<atr_data.length;i++){
+                    obj["atr"+(i)]=obj["atr"+(i+1)]
+                }
+                console.log(obj)
+            })
+        }
+
+        atr_data.forEach((val,key)=>{
+            if(key!=atr_id) tmp.push(val);
+        })
+        setAtrData(tmp)
+    }
+
     return(
         <div>
-            
-            <div className="flex items-center justify-center pt-50  ">
+            <div className="flex items-center justify-center pt-50 ">
+                attribute / details
+            </div>
+            <div className="flex items-center justify-center pt-5">
                 <table>
                     <thead>
                     {atr_data.map((atr,key_atr)=>{
-                        const table_atr="atr"+(key_atr+1).toString();
+                        const table_atr="atr"+(key_atr);
                         return(
                             // atr col: done revamp
                             <tr key={key_atr} className="p-3 bg-amber-100 border-2 border-black">
                                 <th>
-                                    <input onChange={(e)=>{
-                                        AtrOnChange(key_atr,e.target.value);
-                                    }}
+                                    <div className="flex items-center justify-center p-3">
+                                        <input onChange={(e)=>{
+                                            AtrOnChange(key_atr,e.target.value);
+                                        }}
 
-                                    defaultValue={atr} placeholder="Attribute"></input> 
+                                        defaultValue={atr} placeholder="Attribute"></input> 
+                                        <button onClick={()=>HandleTest(key_atr)}>x</button>
+                                    </div>
                                 </th>
 
-                                {table_data.map((obj,key_obj)=>(
-                                    <td key={key_obj} className="p-3 bg-amber-100 border-2 border-black">
+                                {table_data.map((obj,obj_key)=>(
+                                    <td key={obj_key} className="p-3 bg-amber-100 border-2 border-black">
                                         <input onChange={(e)=>{
-                                            HandleCellChange(key_obj,table_atr,e.target.value)
+                                            HandleCellChange(obj_key,table_atr,e.target.value)
                                         }}
                                         value={obj[table_atr] ?? ""} placeholder="Detail">
                                         </input>
@@ -127,9 +135,9 @@ export default function TableDispVert(){
                         );
                     })}
                     </thead>
-                </table>
-
-
+                </table>                
+            </div>
+            <div>
                 <button className="text-black tex-2xl bg-purple-500 p-3 rounded-2xl
                 border-black border-2" onClick={HandleAddCol}>Add Col</button>
 
@@ -137,13 +145,12 @@ export default function TableDispVert(){
                 border-black border-2" onClick={HandleAddRow} >Add Row</button>
 
                 <button className="text-black tex-2xl bg-rose-500 p-3 rounded-2xl
-                border-black border-2" onClick={HandleInitTable}>print</button>
+                border-black border-2" onClick={()=>{
+                    console.log(atr_data.length)
+                    console.log(table_data)}}>print</button>
                 
                 <button className="text-black tex-2xl bg-blue-500 p-3 rounded-2xl border-black border-2" 
                 onClick={HandleSaveTable}>Save</button>
-            
-
-                
             </div>
         </div>
     )
